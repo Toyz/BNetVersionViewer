d8aec216e642c28b5169b37867b9de7f52358e1e
Toyz <toyzz@live.com>
2017-03-22 23:39:59 -0700

    Added the actual code

---
+++ BNetVersionWatcher/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading;
+
+namespace BNetVersionWatcher
+{
+    class Program
+    {
+        static void Main(string[] gameCodes)
+        {
+            Console.Clear();
+            TextWriter oldOut = Console.Out;
+
+            FileStream filestream = new FileStream("out.txt", FileMode.Create);
+            var streamwriter = new StreamWriter(filestream);
+            streamwriter.AutoFlush = true;
+            Console.SetError(streamwriter);
+
+            while (true)
+            {
+                foreach (string game in gameCodes)
+                {
+                    var url = $"http://us.patch.battle.net:1119/{game}/versions?t={DateTime.Now.Millisecond}";
+                    try
+                    {
+                        using (var wc = new WebClient())
+                        {
+                            var data = wc.DownloadString(url).TrimStart().TrimEnd();
+
+                            Console.SetOut(streamwriter);
+                            Console.WriteLine($"Game: {game}{Environment.NewLine}URL: {url}{Environment.NewLine}Contents: {Environment.NewLine}{data}{Environment.NewLine}------");
+                            Console.SetOut(oldOut);
+                            Console.WriteLine($"Game: {game}{Environment.NewLine}URL: {url}{Environment.NewLine}Contents: {Environment.NewLine}{data}{Environment.NewLine}------");
+                        }
+                    }catch(Exception ex)
+                    {
+                        Console.SetOut(streamwriter);
+                        Console.WriteLine($"Game: {game} {Environment.NewLine}URL: {url} {Environment.NewLine}Error: {ex.Message}{Environment.NewLine}------");
+                        Console.SetOut(oldOut);
+                        Console.WriteLine($"Game: {game} {Environment.NewLine}URL: {url} {Environment.NewLine}Error: {ex.Message}{Environment.NewLine}------");
+                    }
+                }
+
+                Thread.Sleep(TimeSpan.FromSeconds(30));
+            }
+        }
+    }
+}
